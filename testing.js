95ac27abd28b9ef061501bda0daca3db47455edf
andy <jia470079834@gmail.com>
2021-02-27 23:45:15 -0500

    string

--- testing.js
+++ testing.js
@@ -494,6 +494,46 @@ function GetPalindromicString(string, start, end){
 	return string.slice(start+1, end)
 }
 
+function groupAnagrams(words) {
+  // Write your code here.
+	let anagrams = {};
+	for (let i = 0; i < words.length; i++){
+		const sortedWord = words[i].split("").sort().join("")
+		if (!anagrams[sortedWord]){
+			anagrams[sortedWord] = [words[i]]
+		} else {
+			anagrams[sortedWord].push(words[i])
+		}
+	}
+	return Object.values(anagrams)
+	
+}
+
+function longestPalindromicSubstring(string) {
+  // Write your code here.
+	let currentLongest = [0, 1];
+	for (let i = 1; i < string.length; i++){
+		const odd = getLongestPalindromeFrom(string, i-1, i+1);
+		const even = getLongestPalindromeFrom(string, i-1, i);
+		const longest = odd[1] - odd[0] > even[1] - even[0] ? odd : even
+		currentLongest = currentLongest[1] - currentLongest[0] > longest[1] - longest[0] ? currentLongest : longest 
+	}
+	return string.slice(currentLongest[0], currentLongest[1])
+}
+
+function getLongestPalindromeFrom(string, left, right){
+	while (left >= 0 && right < string.length){
+		if (string[left] !== string[right]){
+			break;
+		} else {
+			left--;
+			right++;
+		}
+	}
+	return [left+1, right]
+}
+
+
 const string = "2345432abbbba5432zzbbababa";
 console.log(longestPalindromicSubstring(string));
 
