448ab45f1fa968cd4c170108bcc989581131a447
andy <jia470079834@gmail.com>
2021-03-06 23:58:33 -0500

    id

--- testing.js
+++ testing.js
@@ -706,3 +706,19 @@ function reverseWordsInString(string) {
 // Do not edit the line below.
 exports.reverseWordsInString = reverseWordsInString;
 
+function longestSubstringWithoutDuplication(string) {
+  // Write your code here.
+	const lastSeen = {};
+	let longest = [0, 1];
+	let startIdx = 0;
+	for (let i = 0; i < string.length; i++){
+		const char = string[i];
+		if (char in lastSeen){
+			startIdx = Math.max(startIdx, lastSeen[char]);
+		}
+		if (longest[1] - longest[0] < i - startIdx + 1)
+	}
+}
+
+// Do not edit the line below.
+exports.longestSubstringWithoutDuplication = longestSubstringWithoutDuplication;
