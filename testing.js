325321a2b2ba62d746437f1f899ce698d1143777
andy <jia470079834@gmail.com>
2021-03-23 23:42:02 -0400

    prac

--- testing.js
+++ testing.js
@@ -872,3 +872,22 @@ function swapArray(i, j, array) {
 
 // Do not edit the line below.
 exports.moveElementToEnd = moveElementToEnd;
+
+// This is an input class. Do not edit.
+class BinaryTree {
+	constructor(value) {
+		this.value = value;
+		this.left = null;
+		this.right = null;
+	}
+}
+
+function binaryTreeDiameter(tree) {
+	// Write your code here.
+	return -1;
+}
+
+// Do not edit the line below.
+exports.binaryTreeDiameter = binaryTreeDiameter;
+exports.BinaryTree = BinaryTree;
+
