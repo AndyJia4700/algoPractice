e90451b19617b2cb1443cfff8fcb7a222e50d7f7
andy <jia470079834@gmail.com>
2021-04-02 20:47:05 -0400

    prac

--- testing.js
+++ testing.js
@@ -1038,3 +1038,30 @@ function removeDuplicatesFromLinkedList(linkedList) {
 // Do not edit the lines below.
 exports.LinkedList = LinkedList;
 exports.removeDuplicatesFromLinkedList = removeDuplicatesFromLinkedList;
+
+// This is an input class. Do not edit.
+class LinkedList {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+function removeDuplicatesFromLinkedList(linkedList) {
+  // Write your code here.
+	let currentNode = linkedList;
+	while (currentNode){
+		let nextNode = currentNode.next;
+		while (nextNode && nextNode.value == currentNode.value){
+			nextNode = nextNode.next;
+		}
+		
+		currentNode.next = nextNode;
+		currentNode = nextNode;
+	}
+  return linkedList;
+}
+
+// Do not edit the lines below.
+exports.LinkedList = LinkedList;
+exports.removeDuplicatesFromLinkedList = removeDuplicatesFromLinkedList;
