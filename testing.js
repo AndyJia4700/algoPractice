2248ede61e61bc002686eb7dcb33ae822d21680e
andy <jia470079834@gmail.com>
2021-04-09 18:47:32 -0400

    prac

--- testing.js
+++ testing.js
@@ -1184,3 +1184,21 @@ exports.BinaryTree = BinaryTree;
 exports.branchSums = branchSums;
 
 
+function nodeDepths(root, depth = 0) {
+  // Write your code here.
+	if (!root) return 0;
+	return depth + nodeDepths(root.left, depth+1) + nodeDepths(root.right, depth+1)
+	
+}
+
+// This is the class of the input binary tree.
+class BinaryTree {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+// Do not edit the line below.
+exports.nodeDepths = nodeDepths;
