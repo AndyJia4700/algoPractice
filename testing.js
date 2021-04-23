cc0872e78c1075bc35e33275cf1e8a1ceaa00f02
andy <jia470079834@gmail.com>
2021-04-23 13:35:13 -0400

    prac

--- testing.js
+++ testing.js
@@ -1488,3 +1488,24 @@ function longestPeak(array) {
 // Do not edit the line below.
 exports.longestPeak = longestPeak;
 
+function arrayOfProducts(array) {
+  // Write your code here.
+	const products = new Array(array.length).fill(1);
+	
+	let leftProduct = 1;
+	for (let i = 0; i < array.length; i++){
+		products[i] *= leftProduct;
+		leftProduct *= array[i]
+	}
+	
+	let rightProduct = 1;
+	for (let i = array.length - 1; i >= 0; i--){
+		products[i] *= rightProduct;
+		rightProduct *= array[i]
+	}
+	
+	return products
+}
+
+// Do not edit the line below.
+exports.arrayOfProducts = arrayOfProducts;
