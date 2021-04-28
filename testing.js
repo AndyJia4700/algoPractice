1f0e14a030dfec68aefacd0a6f85e548a5c3f812
andy <jia470079834@gmail.com>
2021-04-27 20:55:19 -0400

    prac

--- testing.js
+++ testing.js
@@ -1596,6 +1596,109 @@ class BST {
   	}
 }
 
+// Do not edit the class below except for
+// the insert, contains, and remove methods.
+// Feel free to add new properties and methods
+// to the class.
+class BST {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+
+  insert(value) {
+    // Write your code here.
+		let currentNode = this;
+		while (currentNode){
+			if (currentNode.value > value){
+				//left
+				if (!currentNode.left){
+					currentNode.left = new BST(value);
+					break;
+				} else {
+					currentNode = currentNode.left
+				}
+			} else {
+				//right
+				if (!currentNode.right){
+					currentNode.right = new BST(value);
+					break;
+				} else {
+					currentNode = currentNode.right
+				}
+			}
+		}
+    // Do not edit the return statement of this method.
+    return this;
+  }
+
+  contains(value) {
+    // Write your code here.
+		let currentNode = this;
+		while (currentNode){
+			if (value < currentNode.value){
+				currentNode = currentNode.left
+			} else if (value > currentNode.value){
+				currentNode = currentNode.right
+			} else {
+				return true
+			}
+		}
+		return false
+  }
+
+  remove(value, parentNode = null) {
+    // Write your code here.
+		let currentNode = this;
+		while (currentNode){
+			if (currentNode.value > value){
+				parentNode = currentNode;
+				currentNode = currentNode.left
+			} else if (currentNode.value < value){
+				parentNode = currentNode;
+				currentNode = currentNode.right;
+			} else {
+				if (currentNode.left && currentNode.right){
+					currentNode.value = currentNode.right.getLeastValue();
+					currentNode.right.remove(currentNode.value, currentNode)
+				} else if (!parentNode){
+					if (currentNode.left){
+						currentNode.value = currentNode.left.value;
+						currentNode.right = currentNode.left.right;
+						currentNode.left = currentNode.left.left;
+					} else if (currentNode.right){
+						currentNode.value = currentNode.right.value;
+						currentNode.left = currentNode.right.left;
+						currentNode.right = currentNode.right.right;
+					} else {
+						//single node tree
+					}
+				} else if (parentNode.left == currentNode){
+					parentNode.left = currentNode.left ? currentNode.left : currentNode.right
+				} else if (parentNode.right == currentNode){
+					parentNode.right = currentNode.left ? currentNode.left : currentNode.right
+				}
+				break;
+			}
+		}
+    // Do not edit the return statement of this method.
+    return this;
+  }
+
+	getLeastValue(){
+		let currentNode = this;
+		while (currentNode.left){
+			currentNode = currentNode.left
+		}
+		return currentNode.value
+	}
+}
+
+// Do not edit the line below.
+exports.BST = BST;
+
+
 // Do not edit the line below.
 exports.BST = BST;
 
