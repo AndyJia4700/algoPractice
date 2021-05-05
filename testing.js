673689172f960f675fe72cedaf3da5c24678f5e8
andy <jia470079834@gmail.com>
2021-05-04 22:03:51 -0400

    prac

--- testing.js
+++ testing.js
@@ -1724,3 +1724,41 @@ function inOrderTraverse(node, sortedNodeValues){
 // Do not edit the lines below.
 exports.BST = BST;
 exports.findKthLargestValueInBst = findKthLargestValueInBst;
+
+
+// This is an input class. Do not edit.
+class BST {
+  constructor(value, left = null, right = null) {
+    this.value = value;
+    this.left = left;
+    this.right = right;
+  }
+}
+
+class TreeInfo {
+	constructor(rootIdx){
+		this.rootIdx = rootIdx;
+	}
+}
+
+function reconstructBst(preOrderTraversalValues) {
+  // Write your code here.
+	const treeInfo = new TreeInfo(0);
+	return reconstructBst(-Infinity, Infinity, preOrderTraversalValues, treeInfo);
+}
+
+function reconstructBstFromRange(lowerBound, upperBound, preOrderTraversalValues, currentSubtreeInfo){
+	if (currentSubtreeInfo.rootIdx == preOrderTraversalValues.length) return null;
+	
+	const rootValue = preOrderTraversalValues[currentSubtreeInfo.rootIdx];
+	if (rootValue < lowerBound || rootValue >= upperBound) return null;
+	
+	currentSubtreeInfo.rootIdx++;
+	const leftSubtree = reconstructBstFromRange(lowerBound, rootValue, preOrderTraversalValues, currentSubtreeInfo);
+	const rightSubtree = reconstructBstFromRange(rootValue, upperBound, preOrderTraversalValues, currentSubtreeInfo);
+	return new BST(rootValue, leftSubtree, rightSubtree);
+}
+
+// Do not edit the lines below.
+exports.BST = BST;
+exports.reconstructBst = reconstructBst;
