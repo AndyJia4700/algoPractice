e82a9967b02b326167676f938397eaef531aaad0
andy <jia470079834@gmail.com>
2021-05-10 22:54:03 -0400

    prac

--- testing.js
+++ testing.js
@@ -1826,3 +1826,43 @@ function spiralFill(array, startRow, endRow, startCol, endCol, result){
 // Do not edit the line below.
 exports.spiralTraverse = spiralTraverse;
 
+function minHeightBst(array) {
+  // Write your code here.
+	return helper(array, 0, array.length - 1);
+}
+
+function helper(array, startIdx, endIdx){
+	if (startIdx > endIdx) return null;
+	const midIdx = Math.floor((startIdx + endIdx) / 2);
+	const bst = new BST(array[midIdx]);
+	bst.left = helper(array, startIdx, midIdx - 1);
+	bst.right = helper(array, midIdx + 1, endIdx);
+	return bst;
+}
+
+class BST {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+
+  insert(value) {
+    if (value < this.value) {
+      if (this.left === null) {
+        this.left = new BST(value);
+      } else {
+        this.left.insert(value);
+      }
+    } else {
+      if (this.right === null) {
+        this.right = new BST(value);
+      } else {
+        this.right.insert(value);
+      }
+    }
+  }
+}
+
+// Do not edit the line below.
+exports.minHeightBst = minHeightBst;
